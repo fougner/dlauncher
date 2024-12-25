47ca98bba5a5874675a1269d4a26d16e9d9155f4
Alexander Fougner <fougner89@gmail.com>
2024-12-25 14:09:25 +0100

    Extract UI creation from main into build_ui

--- src/dlauncher.rs
+++ src/dlauncher.rs
@@ -1,5 +1,6 @@
 use dbus_crossroads::{Context, Crossroads, IfaceBuilder};
 use gtk4::{
+  Application,
   glib,
   prelude::*,
 };
@@ -14,27 +15,26 @@ use dlauncher::{
 static PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
 static APP_ID: &str = "net.launchpad.dlauncher";
 
-fn main() {
+fn main() -> glib::ExitCode {
   init_logger();
   debug!("Starting dlauncher...");
-  gtk4::init().expect("Failed to initialize GTK.");
 
+  let application = Application::builder().application_id(APP_ID).build();
+  application.connect_activate(build_ui);
+  application.run()
+}
+
+fn build_ui(app: &Application) {
   let config = Config::read();
-  let application = gtk4::Application::builder().application_id(APP_ID).build();
+  let windows = Window::new(app, &config);
+  windows.build_ui();
+  info!("Started dlauncher v{PKG_VERSION}");
 
-  application.connect_activate(move |application| {
-    let windows = Window::new(application, &config);
-    windows.build_ui();
-    info!("Started dlauncher v{PKG_VERSION}");
-
-    if !config.main.daemon {
-      // skip initializing a dbus interface if daemon isn't enabled & show window
-      windows.window.show();
-      info!("Running in non-daemon mode");
-    }
-
+  if !config.main.daemon {
+    // skip initializing a dbus interface if daemon isn't enabled & show window
     windows.window.show();
-  });
+    info!("Running in non-daemon mode");
+  }
 
-  application.run();
+  windows.window.show();
 }

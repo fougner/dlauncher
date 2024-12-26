0c284251254c83b4e3be3c66228bece86b064f82
Alexander Fougner <fougner89@gmail.com>
2024-12-26 10:40:56 +0100

    dbus toggling works!
    ... but it has some 0.4s delay

--- src/dlauncher.rs
+++ src/dlauncher.rs
@@ -1,24 +1,22 @@
+use dbus::blocking::Connection;
 use dbus_crossroads::{Context, Crossroads, IfaceBuilder};
-use gtk4::{
-  Application,
-  glib,
-  prelude::*,
-};
 
-use log::{debug, info};
+use gtk4::{glib, prelude::*, Application};
 
 use dlauncher::{
   launcher::{util::config::Config, window::Window},
   util::init_logger,
 };
 
+use log::{debug, info};
+
 static PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
 static APP_ID: &str = "net.launchpad.dlauncher";
+static DBUS_NAME: &str = "com.dlauncher.server";
 
 fn main() -> glib::ExitCode {
   init_logger();
   debug!("Starting dlauncher...");
-
   let application = Application::builder().application_id(APP_ID).build();
   application.connect_activate(build_ui);
   application.run()
@@ -34,7 +32,52 @@ fn build_ui(app: &Application) {
     // skip initializing a dbus interface if daemon isn't enabled & show window
     windows.window.show();
     info!("Running in non-daemon mode");
+  } else {
+    schedule_toggle(windows);
   }
+}
 
-  windows.window.show();
+fn schedule_toggle(window: Window) {
+  debug!("Starting dbus interface");
+  let main_context = glib::MainContext::default();
+  let _guard = main_context.acquire().unwrap();
+
+  let (tx, rx) = async_channel::bounded(1);
+
+  std::thread::spawn(move || {
+    let c = Connection::new_session().unwrap();
+    c.request_name(DBUS_NAME, false, true, false)
+      .unwrap();
+    let mut cr = Crossroads::new();
+
+    let iface_token = cr.register(
+      DBUS_NAME,
+      |b: &mut IfaceBuilder<async_channel::Sender<bool>>| {
+        b.method(
+          "OpenWindow",
+          (),
+          (),
+          move |_: &mut Context, thread_tx, (): ()| {
+            debug!("send_blocking");
+            thread_tx.send_blocking(true);
+            Ok(())
+          },
+        );
+      },
+    );
+
+    cr.insert("/open", &[iface_token], tx);
+    cr.serve(&c).unwrap();
+  });
+
+  main_context.spawn_local(async move {
+    debug!("spawn_local for main context");
+    while let Ok(command) = rx.recv().await {
+      if command {
+        debug!("Received message from dbus interface, showing window.");
+        window.toggle_window();
+      }
+    }
+  });
+
 }
